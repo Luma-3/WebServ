078b15958bd90fee4e9fd73dbd8db51704df00b2
Luma-3 <jb.brs@icloud.com>
2024-09-06 20:39:30 +0200

    add clang-tidy config, add exemple, fix cmake

--- sources/main.cpp
+++ sources/main.cpp
@@ -10,10 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+// #include <iostream>
 
-int main(void)
+// int main(void)
+// {
+// 	std::cout << "Hello World!" << std::endl;
+// 	return 0;
+// }
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+void test(int a)
 {
-	std::cout << "Hello World!" << std::endl;
+	if (a = 5) { // Erreur potentielle : affectation au lieu de comparaison
+		std::cout << "a vaut 5" << std::endl;
+	}
+}
+
+int main()
+{
+	int				 x = NULL; // Utilisation de NULL au lieu de nullptr
+	std::vector<int> vec;
+	for (int i = 0; i < 10; i++) { // 10 est un nombre magique
+		vec.push_back(i);
+	}
+
+	int *ptr = (int *)malloc(
+		sizeof(int)); // Utilisation de malloc au lieu de new, non recommandé
+	*ptr = 10;		  // 10 est un nombre magique
+
+	std::cout << "Valeur de ptr : " << *ptr << std::endl;
+	free(ptr); // Utilisation de free au lieu de delete
+
 	return 0;
 }
