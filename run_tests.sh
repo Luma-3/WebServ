411598ae3418a4a6e4e83a48927d2d854ae0bd68
monsieurCanard <anthony.gab@outlook.fr>
2024-11-12 16:21:11 +0100

    Run all_testSSSSSSSSSSSSSSSSSSSSSSS with an SSSSSSSSSS

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+echo "Prepare environment for gtest"
+echo
+
+cp tests/gtest/all_run.sh build/tests/all_run.sh
+
+cp -r tests/gtest/conf/ build/tests/
+chmod 000  build/tests/conf/exception/no_perm.conf
+
+echo "Prepare environment DONE"
+
+echo "Running gtest"
+echo
+
+./build/tests/all_run.sh
+
+echo "Cleaning up"
+echo
+
+chmod 644 build/tests/conf/exception/no_perm.conf
+rm -rf build/tests/conf/
+
+echo "Done"
+
 ./tests/python/run_all_tests_python.sh
 
 echo "Do you want to delete log file generated during the test? [y/n]"
@@ -7,4 +30,3 @@ read answer
 if [ "$answer" == "y" ]; then
 	rm -f log/*.log
 fi
-# run_test "tests/python/run_classic_server.sh" "tests/python/request_return_code.py"
