a9cc7b2079d1c66074c4e8b6308e9444b050ef50
Luma-3 <jb.brs@icloud.com>
2024-11-19 16:44:02 +0100

    test

--- tests/python/www_test/cgi-bin/test.php
+++ tests/python/www_test/cgi-bin/test.php
@@ -1,14 +1,6 @@
 <?php
-$NAME = $_POST['name'] ?? ($_GET['name'] ?? 'no name');
-$age = $_POST['age'] ?? ($_GET['age'] ?? 'no age');
+var_dump($_FILES);
+$NAME = $_POST['name'];
+$age = $_POST['age'];
 echo "name={$NAME}&age={$age}&cookie=";
-
-?>
-
-<?php
-// Retrieve POST body content
-$postBody = file_get_contents('php://input');
-
-// Process the POST body
-echo "Received POST Body: " . $postBody;
 ?>
